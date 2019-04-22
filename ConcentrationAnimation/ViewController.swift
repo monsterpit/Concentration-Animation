b7dde855a473ff58944770b1e443c04b9e7be5d1
MrNoForks <testingiosapp12345@gmail.com>
2019-04-22 16:49:55 +0530

    UIView.transition without completion handler from to (is for animating from 1 view to another view) with (is used to animate within same app)

--- ConcentrationAnimation/ViewController.swift
+++ ConcentrationAnimation/ViewController.swift
@@ -31,10 +31,6 @@ class ViewController: UIViewController {
             cardView.rank = card.rank.order!
             cardView.suit = card.suit.rawValue
             
-            // target this is the object that is going to be sent this action method when the gesture happens
-            // So we are gonna have target to be self the viewcontroller but it could be view , we could send it to view as well
-            //this selector is just hash tag selector and then inside here you put the name of the method but only the external names of the arguments
-            // we gonna have flip card with only 1 argument which is tapGesture with no external name so we gonna have _ : but nothing else
             cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
         }
     }
@@ -43,16 +39,24 @@ class ViewController: UIViewController {
         // first thing we always do is switch on recognizer state
         switch recognizer.state{
         case .ended:
-            //but if i get a tap what i wanna do is get the card that was tapped on but I'm in my view controller
-            //now normally I might have the target have been the view so it would know that it was itself that got tapped on but here I'm in the view controller so I have to find it
-            // well it turn's out UITapGestureRecognizer it knows what view was tapped on
-            //remember I said each of the concrete gesture have information about what happened well one of the things is they know what they were tapped on or whatever
-            // so view is a var on UITapGestureRecognizer which is the view that was tapped on
-            
-            // casting as? PlayCardView because recognizer.view is just a UIView ofcourse UITapGestureRecognizer doesn't know anything about PlayCardView . I have to make sure that it is a fact a PlayCardView it should be because that's the only thing I added any of these tap gestures to
     
             if let chosenCardView = recognizer.view as? PlayCardView{
-                chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+                
+                //from to is when you are transitioning from one view to completely different view
+                //that is you remove 1 from superview and add another one in as a subview
+                // so similiar thing but it's like a card where the back of the card is one view and front of the card is different view
+                
+                //but here we are transitioning with the same view because we can turn it face up and face down
+                
+                UIView.transition(with: chosenCardView,
+                                  duration: 0.6,
+                                  options: .transitionFlipFromLeft,
+                                  animations: {
+                                    chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+                } )
+               //  ,                                 completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+                
+                
             }
         default:
             break
