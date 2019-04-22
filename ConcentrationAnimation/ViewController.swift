f73ab6e36a66b37c975dac254ff1ecc7d4c381c8
MrNoForks <testingiosapp12345@gmail.com>
2019-04-22 18:07:08 +0530

    UIViewPropertyAnimator.runningPropertyAnimator To animate the view getting big and small and fading away

--- ConcentrationAnimation/ViewController.swift
+++ ConcentrationAnimation/ViewController.swift
@@ -39,6 +39,13 @@ class ViewController: UIViewController {
         return cardViews.filter{$0.isFaceUp && !$0.isHidden}
     }
     
+    //bool to say if 2 faceUpCard Match
+    private var faceUpCardViewsMatch : Bool {
+        return faceUpCardViews.count == 2 &&
+    faceUpCardViews[0].rank == faceUpCardViews[1].rank &&
+    faceUpCardViews[0].suit == faceUpCardViews[1].suit
+    }
+    
     @objc func flipCard(_ recognizer : UITapGestureRecognizer){
         // first thing we always do is switch on recognizer state
         switch recognizer.state{
@@ -46,34 +53,61 @@ class ViewController: UIViewController {
     
             if let chosenCardView = recognizer.view as? PlayCardView{
                 
-                //from to is when you are transitioning from one view to completely different view
-                //that is you remove 1 from superview and add another one in as a subview
-                // so similiar thing but it's like a card where the back of the card is one view and front of the card is different view
-                
-                //but here we are transitioning with the same view because we can turn it face up and face down
-                
                 UIView.transition(with: chosenCardView,
                                   duration: 0.6,
                                   options: .transitionFlipFromLeft,
                                   animations: {
                                     chosenCardView.isFaceUp = !chosenCardView.isFaceUp
                                    },
-                                  // It's perfectly fine to have animations in completion handler of other animations
-                                  completion: {finished in
+
+                                  completion: { finished in
+                                    if self.faceUpCardViewsMatch{
+                                        //MARK:- UIViewPropertyAnimator
+                                        UIViewPropertyAnimator.runningPropertyAnimator(
+                                            withDuration: 0.6,
+                                            delay: 0,
+                                            options: [],
+                                            animations: {
+                                                //Only thing I can change in here is those view properties if i change anything else it will do it but it's not going to affect the animation in anyway
+                                                // so what are the animations you wanna do here?
+                                                // well we want all the faceUpCard get really big I am gonna do that with transform it's really easyy to make things big or rotate them
+                                                
+                                                self.faceUpCardViews.forEach{
+                                                    //CGAffineTransform.identity no rotation
+                                                    $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
+                                                }
+                                            },
+                                            // completion has UIViewAnimatingPosition which has position which is either .end .start .current
+                                            // we dont care about it as we dont have animations that are gonna be jumping on top of eaachother here so it's not a problem
+                                            // so here we want another PropertyAnimator
+                                            completion: { (position) in
+                                                UIViewPropertyAnimator.runningPropertyAnimator(
+                                                    //withDuration should be longer than 0.6 because we went from identity transform to 3.0 and now I am going from 3.0 past identity down to 0.1 maybe I want to give it a little extra time go that extra distance it feels like it's coming  out and in same time
+                                                    withDuration: 0.75,
+                                                    delay: 0,
+                                                    options: [],
+                                                    animations: {
+                                                        
+                                                        self.faceUpCardViews.forEach{
+                                                         
+                                                            $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                                                            // fully transparent remember this is gonna happen immediately gonna set to zero but the user is going to see it over te course of withDuration
+                                                            $0.alpha = 0
+                                                        }
+                                                })
+ //                                               ,
+//
+//
+//                                                    //removing the card
+//                                                    completion: { (<#UIViewAnimatingPosition#>) in
+//                                                        <#code#>
+//                                                })
+                                        })
+                                        
+                                    }
                                     
-                                    //Reference to property 'faceUpCardViews' in closure requires explicit 'self.' to make capture semantics explicit
-                                    //swift is saying wait a second bud you are accessing a var on yourself that's gonna capture yourself and I want you to type self. right there so that you realize that you might have loop here a memory cycle
-                                    //because otherwise it is really easy to forget oops this is self and then realize oh I got a memory cycle
-                                    //but do we have a memory cycle here?
-                                    //no we don't
-                                    //because while this closure does capture self
-                                    //self doesn't in any way point to this closure
-                                    //it's not part of any var it's not part of any dictionary or anything that self has
-                                    //it's a closure we are giving off to the animation system so only the animation system has a  pointer to it.So there is no cycle here
-                                    //so no reason for us to do any of those weird local variables
-                                    
-                                    if self.faceUpCardViews.count == 2{
-                                        //so gonna execute a closure forEach of the faceUp cards so that's kinda cool way of doing for loop
+                                    else if self.faceUpCardViews.count == 2{
+
                                         self.faceUpCardViews.forEach{ cardView in
                                             //chosenCardView == $0
                                             UIView.transition(with: cardView,
