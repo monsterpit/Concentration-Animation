f0efeb0a6106af17c7a95e694818e7f071aed84a
MrNoForks <testingiosapp12345@gmail.com>
2019-04-23 12:01:48 +0530

    Animation settles down quickly so lets add new UIDynamicItemBehavior with elasticity (for gaining or losing acceleration)  and resistance (resist forces being applied on it)

--- ConcentrationAnimation/ViewController.swift
+++ ConcentrationAnimation/ViewController.swift
@@ -13,16 +13,12 @@ class ViewController: UIViewController {
     var deck = PlayingCardDeck()
 
     @IBOutlet private var cardViews : [PlayCardView]!
-    
-    //Step1 :- Creating Animator
+
     lazy var animator = UIDynamicAnimator(referenceView: view)
-    
-    //Step2 :- Creating Behavior
-    // executing a var with a closure
+
     lazy var collisionBehavior : UICollisionBehavior = {
        let behavior = UICollisionBehavior()
-        //is have edges of of my reference view keep my cards in I want only thing the cards are gonna bounce off eachother
-        //and so I am gonna do this translatesReferenceBoundsIntoBoundary its kinda a cheap quick way to get a collision boundary
+
         behavior.translatesReferenceBoundsIntoBoundary = true
         
         animator.addBehavior(behavior)
@@ -30,6 +26,28 @@ class ViewController: UIViewController {
         return behavior
     }()
     
+    lazy var itemBehavior : UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        
+        //I dont want those things rotating around
+        behavior.allowsRotation = false
+        
+        //I want elasticity
+        // behavior.elasticity = 1.0 means collision don't lose any energy or gain energy .
+        // if I set behavior.elasticity = 1.1 it will gain energy those things will circle faster and faster then you would forget
+        //but if I said behavior.elasticity = 0.9 they gonna slow down
+        // So behavior.elasticity = 1.0 is this kind of as most elasticity I can give it and not run into an accelerating situation
+        behavior.elasticity = 1.0
+        
+        //which is how much it resist forces being applied on it and I am gonna set that to zero
+        //i don't want any resistance I want to be kind of free flowing and here in outer space and not resisting anything
+        behavior.resistance = 0
+        
+        animator.addBehavior(behavior)
+        
+        return behavior
+        //so now we have to add items to itemBehavior to do it's thing
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,25 +68,30 @@ class ViewController: UIViewController {
             
             cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
             
-            
-            // Step 3 :- Add Item to behavior
             collisionBehavior.addItem(cardView)
-            //now that cardView  is going to instantly going to respect that boundary and start bouncing into other cards but of course we haven't moved to the card it's not moving so we need to do that we need to do that with push behavior
             
-            //UIPushBehavior takes the items you wanna push
+            //has soon as we add this it will have all those setting
+            itemBehavior.addItem(cardView)
+            
             let push = UIPushBehavior(items: [cardView], mode: .instantaneous)
-            //angle between 0 and 2pi radians
+
             push.angle = (2*CGFloat.pi).arc4random
             
             push.magnitude = CGFloat(1.0) + CGFloat(2.0).arc4random
             
-            //this cause memory cycle because push inside closure is kept in memory by closure and ofcourse push.action is pointing to the closure so they are keeping eachother in memory so we are getting rid of these by saying unowned 
             push.action = { [unowned push] in
                 push.dynamicAnimator?.removeBehavior(push)
             }
             
-            //adding push behavior to animator
             animator.addBehavior(push)
+            
+            // the game is kinda easy it settles down quickly it settles down it kinda settles down too easily too quickly that's gonna make it too easy to play this game
+            //all of those cards moving a little more
+            // other thing I don't think I want them rotated it's kinda a fun to have them rotated actually
+            // but when you flip the card for e.g. it doesn't flip along the axis of the drawing it flips along the axis of the view
+            // so tilted cards flips around the corner may be that's okay but we are going to use dynamic item behavior
+            //also the reason it is slowing down so much is there's not enough elasticity in the collision
+            // So I want those collisions bouncing off and keeping energy up and we will do that with another kind of behavior
         }
     }
     
@@ -105,21 +128,16 @@ class ViewController: UIViewController {
                                             delay: 0,
                                             options: [],
                                             animations: {
-                                                //Only thing I can change in here is those view properties if i change anything else it will do it but it's not going to affect the animation in anyway
-                                                // so what are the animations you wanna do here?
-                                                // well we want all the faceUpCard get really big I am gonna do that with transform it's really easyy to make things big or rotate them
                                                 
                                                 self.faceUpCardViews.forEach{
                                                     //CGAffineTransform.identity no rotation
                                                     $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
                                                 }
                                             },
-                                            // completion has UIViewAnimatingPosition which has position which is either .end .start .current
-                                            // we dont care about it as we dont have animations that are gonna be jumping on top of eaachother here so it's not a problem
-                                            // so here we want another PropertyAnimator
+
                                             completion: { (position) in
                                                 UIViewPropertyAnimator.runningPropertyAnimator(
-                                                    //withDuration should be longer than 0.6 because we went from identity transform to 3.0 and now I am going from 3.0 past identity down to 0.1 maybe I want to give it a little extra time go that extra distance it feels like it's coming  out and in same time
+
                                                     withDuration: 0.75,
                                                     delay: 0,
                                                     options: [],
@@ -128,7 +146,7 @@ class ViewController: UIViewController {
                                                         self.faceUpCardViews.forEach{
                                                          
                                                             $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
-                                                            // fully transparent remember this is gonna happen immediately gonna set to zero but the user is going to see it over te course of withDuration
+
                                                             $0.alpha = 0
                                                         }
                                                 },
@@ -153,7 +171,7 @@ class ViewController: UIViewController {
                                     else if self.faceUpCardViews.count == 2{
 
                                         self.faceUpCardViews.forEach{ cardView in
-                                            //chosenCardView == $0
+   
                                             UIView.transition(with: cardView,
                                                               duration: 0.6,
                                                               options: .transitionFlipFromLeft,
